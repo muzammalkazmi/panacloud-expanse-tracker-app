e08654228cb367150d85eb6e0e6c33320e68674b
muzammal kazmi <muzammalkazmi@gmail.com>
2021-09-22 11:57:04 +0500

    dynamically added plus and minus classes

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default function Transaction({ transaction }) {
+    const sign= transaction.amount>0?'+':'-';
+    const transactionType=transaction.amount>0?'plus':'minus'
     return ( 
-    <div >
-        <li > { transaction.detail } < span > { transaction.amount } </span> <button type='button'className='dlt-btn'>X</button>
+    <div className={transactionType}>
+        <li > { transaction.detail } < span > ${sign}{ transaction.amount } </span>
+         <button type='button'className='dlt-btn'>X</button>
         </li> 
         </div>
     )
